7d6568503343f198664a1e461399d7ba1c6fb349
justplay0526 <justplay0526@gmail.com>
2022-12-17 23:03:13 +0800

    add CH9_1

--- CH8/app/src/main/java/com/example/lab8_kotlin/SecActivity.kt
+++ CH8/app/src/main/java/com/example/lab8_kotlin/SecActivity.kt
@@ -12,13 +12,13 @@ class SecActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sec)
-        //將變數與 XML 元件綁定
+
         val btn_send = findViewById<Button>(R.id.btn_send)
         val ed_name = findViewById<EditText>(R.id.ed_name)
         val ed_phone = findViewById<EditText>(R.id.ed_phone)
-        //設定按鈕監聽器
+
         btn_send.setOnClickListener {
-            //判斷是否輸入資料
+
             when {
                 ed_name.length() < 1 -> showToast("請輸入姓名")
                 ed_phone.length() < 1 -> showToast("請輸入電話")
@@ -26,7 +26,7 @@ class SecActivity : AppCompatActivity() {
                     val b = Bundle()
                     b.putString("name", ed_name.text.toString())
                     b.putString("phone", ed_phone.text.toString())
-                    //使用 setResult()回傳聯絡人資料
+
                     setResult(Activity.RESULT_OK, Intent().putExtras(b))
                     finish()
                 }
